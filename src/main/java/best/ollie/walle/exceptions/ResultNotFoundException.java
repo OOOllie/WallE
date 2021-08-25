031ed9817041760adb7d837289728fbb50bc8b0e
OOOllie <56447841+OOOllie@users.noreply.github.com>
2021-08-25 14:28:47 +0100

    Added comments for the ResultNotFoundException class

--- src/main/java/best/ollie/walle/exceptions/ResultNotFoundException.java
+++ src/main/java/best/ollie/walle/exceptions/ResultNotFoundException.java
@@ -1,7 +1,13 @@
 package best.ollie.walle.exceptions;
 
+/**
+ * Exception for when database returns no result
+ */
 public class ResultNotFoundException extends Exception {
 
+	/**
+	 * @param message Exact error message as to what happened for the database
+	 */
 	public ResultNotFoundException(String message) {
 		super(message);
 	}
